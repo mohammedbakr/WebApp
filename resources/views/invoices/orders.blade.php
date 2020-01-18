67a79b5fc72801d61560c5688b13784d56931540
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-01-18 12:41:46 +0200

    Add style for Invoice

--- resources/views/invoices/orders.blade.php
+++ resources/views/invoices/orders.blade.php
@@ -7,7 +7,6 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Order Invoice</title>
-    {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"> --}}
     <style type="text/css">
         table { border-collapse: collapse;}
     </style>
@@ -15,8 +14,8 @@
 <body>
     <section class="row">
         <div class="pull-left">
-            Invoice to: {{$customer->name}} <br />
-            Deliver to: <strong>{{ $address->alias }} <br /></strong>
+            Invoice to: <strong>{{$customer->name}}</strong> <br />
+            Deliver to: <strong>{{ $address->alias }}</strong><br />
             {{ $address->address_1 }} {{ $address->address_2 }} <br />
             {{ $address->city }} {{ $address->province }} <br />
             {{ $address->country }} {{ $address->zip }}
@@ -24,12 +23,13 @@
         <div class="pull-right">
             From: {{config('app.name')}}
         </div>
+        <hr width="100%" color="#CCC"/>
     </section>
     <section class="row">
         <div class="col-md-12">
             <h2>Details</h2>
-            <table class="table table-striped" width="100%" border="0" cellspacing="0" cellpadding="0">
-                <thead>
+            <table class="table table-striped" width="100%" border="0.3" cellspacing="10" cellpadding="10" style="border:1px solid #CCC;">
+                <thead style="background-color:#DDD;">
                     <tr>
                         <th>SKU</th>
                         <th>Name</th>
